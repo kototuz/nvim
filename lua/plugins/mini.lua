66d1a9aaf47522078611ce7e974618a9d3ead94a
kototuz <ivann1812@gmail.com>
2025-06-14 21:22:19 +0300

    Add mini.align

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,4 +1,5 @@
 return {
-    { 'echasnovski/mini.operators', version = '*',  config = function() require('mini.operators').setup() end },
-    { 'echasnovski/mini.surround', version = '*',   config = function() require('mini.surround').setup() end },
+    { 'echasnovski/mini.operators', version = '*', config = function() require('mini.operators').setup() end },
+    { 'echasnovski/mini.surround',  version = '*', config = function() require('mini.surround').setup()  end },
+    { 'echasnovski/mini.align',     version = '*', config = function() require('mini.align').setup()     end },
 }
