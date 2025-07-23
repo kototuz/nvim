79b2291d75ee55b4ade582c4d6763b6973d3ddcf
kototuz <ivann1812@gmail.com>
2025-07-23 22:43:37 +0300

    Add several keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,7 +15,7 @@ return {
 
         local builtin = require("telescope.builtin")
         vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>b', builtin.buffers, {})
     end
 }
