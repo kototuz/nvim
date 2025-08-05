801c314888395e11377904e09fff9be3ee0d9fe0
kototuz <kototuzz@gmail.com>
2025-08-05 22:59:43 +0300

    Colorscheme

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,18 +1,23 @@
 return {
-    "metalelf0/black-metal-theme-neovim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("black-metal").setup {
-            theme = "taake",
-            transparent = true,
-            variant = "dark",
-            highlights = {
-                ["@punctuation.bracket"] = { fg = "$fg" },
-                ["@constructor.lua"]     = { fg = "$fg" }
+    {
+        "metalelf0/black-metal-theme-neovim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("black-metal").setup {
+                theme = "taake",
+                variant = "dark",
+                transparent = true,
+                code_style = {
+                    comments = "none"
+                },
+                highlights = {
+                    ["@punctuation.bracket"] = { fg = "$fg" },
+                    ["@constructor.lua"]     = { fg = "$fg" }
+                }
             }
-        }
 
-        require("black-metal").load()
-    end
+            require("black-metal").load()
+        end
+    },
 }
