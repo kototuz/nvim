e1c669ce9076f01e3832aa311d9e709abcc2d4b9
kototuz <kototuzz@gmail.com>
2025-08-05 22:59:22 +0300

    Terminal

--- lua/terminal.lua
+++ lua/terminal.lua
@@ -1,26 +1,17 @@
---
--- local group = vim.api.nvim_create_augroup("custom-term-open", {})
--- vim.api.nvim_create_autocmd("TermOpen", {
---     group = group,
---     callback = function()
-    --     local set = vim.opt_local
---         set.number = false
---         set.relativenumber = false
---         set.scrolloff = 0
---         vim.bo.bufhidden = "delete"
---         vim.bo.filetype = "terminal"
---         vim.cmd.normal("i")
---     end,
--- })
+vim.api.nvim_create_autocmd("TermOpen", {
+    group = vim.api.nvim_create_augroup("custom-term-open", {}),
+    callback = function()
+        local set = vim.opt_local
+        set.number = false
+        set.relativenumber = false
+        set.scrolloff = 0
+        vim.bo.bufhidden = "delete"
+        vim.bo.filetype = "terminal"
+    end,
+})
 
-vim.keymap.set("t", "<c-c><c-c>", "<c-\\><c-n>")
+vim.keymap.set("t", "<C-c>", "<c-\\><c-n>")
 vim.keymap.set("n", "<leader>t", function()
     vim.cmd.terminal()
     vim.cmd.normal("i")
-    local set = vim.opt_local
-    set.number = false
-    set.relativenumber = false
-    set.scrolloff = 0
-    vim.bo.bufhidden = "delete"
-    vim.bo.filetype = "terminal"
 end)
